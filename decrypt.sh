d69805a5ebc7aaa6c744fa3d7129ee53a407e7c2
Rodrigo Toste Gomes <rodrigo.toste.gomes@gmail.com>
2016-02-23 23:12:03 -0500

    Better encryption

--- decrypt.sh
+++ decrypt.sh
@@ -4,4 +4,8 @@
 # http://askubuntu.com/questions/27770/is-there-a-tool-to-encrypt-a-file-or-directory
 # Originally encrypted with:
 # gpg --symmetric < confidential.tar.xz > confidential.tar.xz.enc
-gpg --decrypt < confidential.tar.xz.enc > confidential.tar.xz
+# Then I saw this post:
+# http://superuser.com/questions/633715/how-do-i-fix-warning-message-was-not-integrity-protected-when-using-gpg-symme
+# And I encrypted with:
+# gpg -o confidential.tar.xz.enc --cipher-algo AES256 --symmetric confidential.tar.xz
+gpg -o confidential.tar.xz --decrypt confidential.tar.xz.enc
