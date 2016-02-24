a04cee1172957b5b8415674cc4e62a4969ab78af
Rodrigo Toste Gomes <rodrigo.toste.gomes@gmail.com>
2016-02-24 01:20:17 -0500

    Fixing up client.sh: simplification + corrections to how to clean up

--- client.sh
+++ client.sh
@@ -11,21 +11,18 @@ ROOT_CA_FILE="./rootCA.pem"
 tmpdir=$(mktemp -d)
 if ! mkfifo "$tmpdir/sin"
 then
-    echo "Attacker?"
+    echo "Strange thing happening? Can't write to tmp? Attacker spoofing fifo?"
     exit 1;
 fi
 
 function on_exit()
 {
-    echo "Cleaning up FIFOs and background processes"
+    echo "Cleaning up FIFOs and children"
+    kill `jobs -p`
     rm -rf "$tmpdir"
-    for j in `jobs -p`
-    do
-        kill $j
-    done
+    exit 0
 }
 trap 'on_exit' EXIT
-
 
 # The condition in this loop is based on
 # http://unix.stackexchange.com/questions/53641/how-to-make-bidirectional-pipe-between-two-programs
@@ -33,17 +30,15 @@ trap 'on_exit' EXIT
 # -verify and -verify_return_error guarantee we fail the connection if server
 # does not have validly signed certificate. There so no one can hijack our
 # machines unless we allow it
-while ! (
-        openssl s_client -connect sicp-s4.mit.edu:6601 -quiet \
-                -verify 0 -verify_return_error -CAfile $ROOT_CA_FILE \
-                2> /dev/null < "$tmpdir/sin" |
-            bash > "$tmpdir/sin";
-        false
-    )
+
+while true
 do
+    openssl s_client -connect sicp-s4.mit.edu:6601 -quiet \
+            -verify 0 -verify_return_error -CAfile $ROOT_CA_FILE \
+            2> /dev/null < "$tmpdir/sin" | \
+        bash > "$tmpdir/sin"
     # Retry every second
     # Assumes temporary internet shortage.
     echo "Retrying"
     sleep 1
 done
-openssl s_client -connect sicp-s4.mit.edu:6601 -quiet 2> /dev/null 1> sin < sout & bash < sin > sout
